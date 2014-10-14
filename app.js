79b0c66274b5a5d9366a40b6c116414feaa21d4a
nonogon <leslie.strauss@gmail.com>
2014-10-14 14:43:28 -0700

    Generator is functioning.

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 var App = Ember.Application.create ({
 });
+
+App.IndexController = Ember.ObjectController.extend ({
+  favoriteColor: '',
+  streetName: '',
+  name: '',
+
+  stageName: function(key, value, previousValue) {
+    if (arguments > 2) {
+      var stageNameParts = value.split(/\s+/);
+      this.set('favoriteColor', stageNameParts[0]);
+      this.set('streetName', stageNameParts[1]);
+      this.set('name', stageNameParts[2]);
+    }
+
+    return this.get('favoriteColor') + ' ' + this.get('streetName') + ' ' + this.get('name')
+    }.property('favoriteColor', 'streetName', 'name')
+});
